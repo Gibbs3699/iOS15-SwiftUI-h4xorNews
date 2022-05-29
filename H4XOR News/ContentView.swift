8e4cf9a820cc8e0e5f4455cb55ebb027b128ead2
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-29 22:10:23 +0700

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by TheGIZzz on 29/5/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
