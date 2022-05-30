db70fe6d305988d0617bbebed147a1cb783ed126
Gibbs3699 <gift_kamonpan@hotmail.com>
2022-05-30 08:16:55 +0700

    Showing detail in webView

---
+++ H4XOR News/View/DetailView.swift
@@ -0,0 +1,26 @@
+//
+//  DetailView.swift
+//  H4XOR News
+//
+//  Created by TheGIZzz on 30/5/2565 BE.
+//
+
+import SwiftUI
+import WebKit
+
+struct DetailView: View {
+    
+    let url: String?
+    
+    var body: some View {
+        WebView(urlString: url)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(url: "https://www.google.com")
+    }
+}
+
+
---
+++ H4XOR News/View/WebView.swift
@@ -0,0 +1,28 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by TheGIZzz on 30/5/2565 BE.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
